ef8cd211a6b22fcbb2063bf2c2884a0f5abba366
Menezess42 <arielmenezess@hotmail.com>
2025-02-01 00:58:25 -0300

    feat: start ownMoney routes

---
+++ MezPlanner/backend/blueprints/ownMoney/routes.py
@@ -0,0 +1,15 @@
+from flask import Blueprint, request, jsonify
+from blueprints.ownMoney.models import OwnMoney
+from backend.blueprints.app import db
+
+ownMoney = Blueprint("ownMoeny", __name__)
+
+# Create
+
+# Read
+
+# Update
+
+# Delete
+
+
