f5c3caf7e9568fceb95df4f2b2f548d08fc5faf9
Menezess42 <arielmenezess@hotmail.com>
2025-02-18 19:04:35 -0300

    no comment

---
+++ MezPlanner/frontend/MezPlanner-Front/src/src/App.jsx
@@ -0,0 +1,11 @@
+import '../styles/App.css'
+import Header from './Header.jsx'
+function App() {
+  return (
+    <>
+      <Header/>
+    </>
+  )
+}
+
+export default App
---
+++ MezPlanner/frontend/MezPlanner-Front/src/src/Header.jsx
@@ -0,0 +1,13 @@
+import '../styles/Header.css';
+
+function Header() {
+  return (
+    <header className="header">
+      <h1>MezPlanner</h1>
+      <div> <h2> User </h2> </div>
+    </header>
+  );
+}
+
+export default Header;
+
