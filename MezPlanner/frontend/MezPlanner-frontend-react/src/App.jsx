0964e2ab27ca8712da82d667ae293cd0ad586663
Menezess42 <arielmenezess@hotmail.com>
2025-02-18 10:01:26 -0300

    restarting frontend

--- MezPlanner/frontend/MezPlanner-frontend-react/src/App.jsx
+++
@@ -1,9 +0,0 @@
-// App is the main component. App is responsible to ty every component together.
-import Header from './Header.jsx'
-function App() {
-    return(
-        <Header/>
-    );
-}
-
-export default App
